ffce5cd0986388f2639160728a86835004e6afed
Eric Thomas <bluebird6922@gmail.com>
2022-09-07 12:39:47 +0300

    progressive image loading

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,99 +1,143 @@
+import React from "react";
 import { motion } from "framer-motion";
+import useProgressiveImg from "../hooks/useProgressiveImg";
 
-function Team() {
-  const utilityCards = [
-    {
-      imgSrc: "images/team/mem3.png",
-      title: "MICHAEL PIERI",
-      subtitle: "FOUNDER & CEO",
-      detailTable: [
-        { key: "EXPERIENCE", value: "REAL ESTATE, BUSINESS OWNER" },
-        { key: "SOCIALS", value: "TWITTER, LINKEDIN, YOUTUBE, INSTAGRAM" },
-        { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
-      ],
-    },
+const utilityCards = [
+  {
+    imgSrc: "images/team/mem3.png",
+    title: "MICHAEL PIERI",
+    subtitle: "FOUNDER & CEO",
+    detailTable: [
+      { key: "EXPERIENCE", value: "REAL ESTATE, BUSINESS OWNER" },
+      { key: "SOCIALS", value: "TWITTER, LINKEDIN, YOUTUBE, INSTAGRAM" },
+      { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
+    ],
+  },
 
-    {
-      imgSrc: "images/team/mem2.png",
-      title: "ISAAC WIESENTHAL",
-      subtitle: "CHIEF MARKETING OFFICER",
-      detailTable: [
-        { key: "EXPERIENCE", value: "COMPETETIVE BOXER, MARKETING GENIUS" },
-        { key: "SOCIALS", value: "LINKEDIN, INSTAGRAM" },
-        { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
-      ],
-    },
-    {
-      imgSrc: "images/team/mem1.png",
-      title: "MICHAEL PIERI",
-      subtitle: "HEAD OF MARKETING",
-      detailTable: [
-        { key: "EXPERIENCE", value: "REAL ESTATE AGENT, BUSINESS OWNER" },
-        { key: "SOCIALS", value: "TWITTER, LINKEDIN, YOUTUBE, INSTAGRAM" },
-        { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
-      ],
-    },
-    {
-      imgSrc: "images/team/mem4.png",
-      title: "MANON",
-      subtitle: "LEAD ARTIST",
-      detailTable: [
-        { key: "EXPERIENCE", value: "ARTIST" },
-        { key: "SOCIALS", value: "LINKEDIN, INSTAGRAM" },
-        { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
-      ],
-    },
-    {
-      imgSrc: "images/team/mem5.png",
-      title: "ANIS AMRANE",
-      subtitle: "LEAD ARTIST",
-      detailTable: [
-        { key: "EXPERIENCE", value: "ARTIST" },
-        { key: "SOCIALS", value: "LINKEDIN, INSTAGRAM" },
-        { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
-      ],
-    },
-    {
-      imgSrc: "images/team/mem5.png",
-      title: "ANIS AMRANE",
-      subtitle: "LEAD ARTIST",
-      detailTable: [
-        { key: "EXPERIENCE", value: "ARTIST" },
-        { key: "SOCIALS", value: "LINKEDIN, INSTAGRAM" },
-        { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
-      ],
-    },
-    {
-      imgSrc: "images/team/mem3.png",
-      title: "MICHAEL PIERI",
-      subtitle: "FOUNDER & CEO",
-      detailTable: [
-        { key: "EXPERIENCE", value: "REAL ESTATE, BUSINESS OWNER" },
-        { key: "SOCIALS", value: "TWITTER, LINKEDIN, YOUTUBE, INSTAGRAM" },
-        { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
-      ],
-    },
-    {
-      imgSrc: "images/team/mem3.png",
-      title: "MICHAEL PIERI",
-      subtitle: "FOUNDER & CEO",
-      detailTable: [
-        { key: "EXPERIENCE", value: "REAL ESTATE, BUSINESS OWNER" },
-        { key: "SOCIALS", value: "TWITTER, LINKEDIN, YOUTUBE, INSTAGRAM" },
-        { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
-      ],
-    },
-    {
-      imgSrc: "images/team/mem3.png",
-      title: "MICHAEL PIERI",
-      subtitle: "FOUNDER & CEO",
-      detailTable: [
-        { key: "EXPERIENCE", value: "REAL ESTATE, BUSINESS OWNER" },
-        { key: "SOCIALS", value: "TWITTER, LINKEDIN, YOUTUBE, INSTAGRAM" },
-        { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
-      ],
-    },
-  ];
+  {
+    imgSrc: "images/team/mem2.png",
+    title: "ISAAC WIESENTHAL",
+    subtitle: "CHIEF MARKETING OFFICER",
+    detailTable: [
+      { key: "EXPERIENCE", value: "COMPETETIVE BOXER, MARKETING GENIUS" },
+      { key: "SOCIALS", value: "LINKEDIN, INSTAGRAM" },
+      { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
+    ],
+  },
+  {
+    imgSrc: "images/team/mem1.png",
+    title: "MICHAEL PIERI",
+    subtitle: "HEAD OF MARKETING",
+    detailTable: [
+      { key: "EXPERIENCE", value: "REAL ESTATE AGENT, BUSINESS OWNER" },
+      { key: "SOCIALS", value: "TWITTER, LINKEDIN, YOUTUBE, INSTAGRAM" },
+      { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
+    ],
+  },
+  {
+    imgSrc: "images/team/mem4.png",
+    title: "MANON",
+    subtitle: "LEAD ARTIST",
+    detailTable: [
+      { key: "EXPERIENCE", value: "ARTIST" },
+      { key: "SOCIALS", value: "LINKEDIN, INSTAGRAM" },
+      { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
+    ],
+  },
+  {
+    imgSrc: "images/team/mem5.png",
+    title: "ANIS AMRANE",
+    subtitle: "LEAD ARTIST",
+    detailTable: [
+      { key: "EXPERIENCE", value: "ARTIST" },
+      { key: "SOCIALS", value: "LINKEDIN, INSTAGRAM" },
+      { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
+    ],
+  },
+  {
+    imgSrc: "images/team/mem5.png",
+    title: "ANIS AMRANE",
+    subtitle: "LEAD ARTIST",
+    detailTable: [
+      { key: "EXPERIENCE", value: "ARTIST" },
+      { key: "SOCIALS", value: "LINKEDIN, INSTAGRAM" },
+      { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
+    ],
+  },
+  {
+    imgSrc: "images/team/mem3.png",
+    title: "MICHAEL PIERI",
+    subtitle: "FOUNDER & CEO",
+    detailTable: [
+      { key: "EXPERIENCE", value: "REAL ESTATE, BUSINESS OWNER" },
+      { key: "SOCIALS", value: "TWITTER, LINKEDIN, YOUTUBE, INSTAGRAM" },
+      { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
+    ],
+  },
+  {
+    imgSrc: "images/team/mem3.png",
+    title: "MICHAEL PIERI",
+    subtitle: "FOUNDER & CEO",
+    detailTable: [
+      { key: "EXPERIENCE", value: "REAL ESTATE, BUSINESS OWNER" },
+      { key: "SOCIALS", value: "TWITTER, LINKEDIN, YOUTUBE, INSTAGRAM" },
+      { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
+    ],
+  },
+  {
+    imgSrc: "images/team/mem3.png",
+    title: "MICHAEL PIERI",
+    subtitle: "FOUNDER & CEO",
+    detailTable: [
+      { key: "EXPERIENCE", value: "REAL ESTATE, BUSINESS OWNER" },
+      { key: "SOCIALS", value: "TWITTER, LINKEDIN, YOUTUBE, INSTAGRAM" },
+      { key: "FAVOURITE QUOTE", value: "ETHERIUM" },
+    ],
+  },
+];
+
+const TeamCard = ({ card }) => {
+  const [src, { blur }] = useProgressiveImg(
+    "images/team/tiny.jpg",
+    card.imgSrc
+  );
+
+  return (
+    <div className="relative flex-col flex w-full">
+      <div className="border-stone-30 border-2 relative">
+        <div style={{ minHeight: "500px" }}>
+          <img
+            src={src}
+            className="w-full"
+            style={{
+              filter: blur ? "blur(20px)" : "none",
+              transition: blur ? "none" : "filter 0.3s ease-out",
+            }}
+            alt="Team"
+          />
+        </div>
+        <div className="flex pb-4 absolute bottom-0 pl-2 backdrop-blur-xl w-full">
+          <div className="flex flex-col">
+            <h1 className="w-5/6 pt-2 font-semibold">{card.title}</h1>
+            <h2 className="pb-2">{card.subtitle}</h2>
+            <table className="text-xs">
+              {card.detailTable.map((row) => (
+                <tr>
+                  <td className="pr-3 font-extralight opacity-100">
+                    {row.key}
+                  </td>
+                  <td>{row.value}</td>
+                </tr>
+              ))}
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Team = () => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -112,29 +156,7 @@ function Team() {
         </div>
         <div class="grid xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 xl:gap-24 md:gap-20 mt-8">
           {utilityCards.map((card) => (
-            <div className="relative flex-col flex w-full">
-              <div className="border-stone-30 border-2 relative">
-                <div style={{ minHeight: "500px" }}>
-                  <img src={card.imgSrc} alt="NFT" className="w-full" />
-                </div>
-                <div className="flex pb-4 absolute bottom-0 pl-2 backdrop-blur-xl w-full">
-                  <div className="flex flex-col">
-                    <h1 className="w-5/6 pt-2 font-semibold">{card.title}</h1>
-                    <h2 className="pb-2">{card.subtitle}</h2>
-                    <table className="text-xs">
-                      {card.detailTable.map((row) => (
-                        <tr>
-                          <td className="pr-3 font-extralight opacity-100">
-                            {row.key}
-                          </td>
-                          <td>{row.value}</td>
-                        </tr>
-                      ))}
-                    </table>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <TeamCard card={card}></TeamCard>
           ))}
         </div>
 
@@ -174,6 +196,6 @@ function Team() {
       </div>
     </motion.div>
   );
-}
+};
 
 export default Team;
--- src/components/Utility.js
+++ src/components/Utility.js
@@ -1,69 +1,114 @@
+import React from "react";
 import { motion } from "framer-motion";
+import useProgressiveImg from "../hooks/useProgressiveImg";
 
-function Utility() {
-  const utilityCards = [
-    {
-      number: "#01",
-      imgSrc: "images/utility/NFT1.png", //https://choosen-ones-dev.netlify.app/
-      title: "WORLD CLASS ART WITH FULL OWNERSHIP",
-      detailTable: [
-        { key: "ARTWORK", value: "FULL OWNERSHIP" },
-        { key: "TOKEN STANDARD", value: "ERC" },
-        { key: "BLOCKCHAIN", value: "ETH & SOL" },
-      ],
-    },
-    {
-      number: "#02",
-      imgSrc: "images/utility/NFT2.png",
-      title: "DIGITAL ATM CARD EXCLUSIVE ACCESS TO EVENTS",
-      detailTable: [
-        { key: "ARTWORK", value: "FULL OWNERSHIP" },
-        { key: "TOKEN STANDARD", value: "ERC" },
-        { key: "BLOCKCHAIN", value: "ETH & SOL" },
-      ],
-    },
-    {
-      number: "#03",
-      imgSrc: "images/utility/NFT3.png",
-      title: "MONTHLY REWARDS AND GIVEAWAYS",
-      detailTable: [
-        { key: "DISCORD", value: "GIVEAWAYS" },
-        { key: "TWITTER", value: "COMPETITIONS" },
-        { key: "BLOCKCHAIN", value: "ETH & SOL" },
-      ],
-    },
-    {
-      number: "#04",
-      imgSrc: "images/utility/NFT4.png",
-      title: "EVENTS, WORKSHOPS & MASTERCLASS",
-      detailTable: [
-        { key: "EVENTS", value: "UNRIVALLED" },
-        { key: "TOKEN STANDARD", value: "ERC111" },
-        { key: "BLOCKCHAIN", value: "ETH & SOL" },
-      ],
-    },
-    {
-      number: "#05",
-      imgSrc: "images/utility/NFT5.png",
-      title: "AI DRIVEN AUTOMATION TOOL CREATE TO EARN ACCELERATOR",
-      detailTable: [
-        { key: "ARTWORK", value: "FULL OWNERSHIP" },
-        { key: "TOKEN STANDARD", value: "ERC" },
-        { key: "BLOCKCHAIN", value: "ETH & SOL" },
-      ],
-    },
-    {
-      number: "#06",
-      imgSrc: "images/utility/NFT6.png",
-      title: "CASCADE TAX PLATFORM",
-      detailTable: [
-        { key: "EVENTS", value: "UNRIVALLED" },
-        { key: "TOKEN STANDARD", value: "ERC111" },
-        { key: "BLOCKCHAIN", value: "ETH & SOL" },
-      ],
-    },
-  ];
+const utilityCards = [
+  {
+    number: "#01",
+    imgSrc: "images/utility/NFT1.jpg", //https://choosen-ones-dev.netlify.app/
+    title: "WORLD CLASS ART WITH FULL OWNERSHIP",
+    detailTable: [
+      { key: "ARTWORK", value: "FULL OWNERSHIP" },
+      { key: "TOKEN STANDARD", value: "ERC" },
+      { key: "BLOCKCHAIN", value: "ETH & SOL" },
+    ],
+  },
+  {
+    number: "#02",
+    imgSrc: "images/utility/NFT2.jpg",
+    title: "DIGITAL ATM CARD EXCLUSIVE ACCESS TO EVENTS",
+    detailTable: [
+      { key: "ARTWORK", value: "FULL OWNERSHIP" },
+      { key: "TOKEN STANDARD", value: "ERC" },
+      { key: "BLOCKCHAIN", value: "ETH & SOL" },
+    ],
+  },
+  {
+    number: "#03",
+    imgSrc: "images/utility/NFT3.jpg",
+    title: "MONTHLY REWARDS AND GIVEAWAYS",
+    detailTable: [
+      { key: "DISCORD", value: "GIVEAWAYS" },
+      { key: "TWITTER", value: "COMPETITIONS" },
+      { key: "BLOCKCHAIN", value: "ETH & SOL" },
+    ],
+  },
+  {
+    number: "#04",
+    imgSrc: "images/utility/NFT4.jpg",
+    title: "EVENTS, WORKSHOPS & MASTERCLASS",
+    detailTable: [
+      { key: "EVENTS", value: "UNRIVALLED" },
+      { key: "TOKEN STANDARD", value: "ERC111" },
+      { key: "BLOCKCHAIN", value: "ETH & SOL" },
+    ],
+  },
+  {
+    number: "#05",
+    imgSrc: "images/utility/NFT5.jpg",
+    title: "AI DRIVEN AUTOMATION TOOL CREATE TO EARN ACCELERATOR",
+    detailTable: [
+      { key: "ARTWORK", value: "FULL OWNERSHIP" },
+      { key: "TOKEN STANDARD", value: "ERC" },
+      { key: "BLOCKCHAIN", value: "ETH & SOL" },
+    ],
+  },
+  {
+    number: "#06",
+    imgSrc: "images/utility/NFT6.jpg",
+    title: "CASCADE TAX PLATFORM",
+    detailTable: [
+      { key: "EVENTS", value: "UNRIVALLED" },
+      { key: "TOKEN STANDARD", value: "ERC111" },
+      { key: "BLOCKCHAIN", value: "ETH & SOL" },
+    ],
+  },
+];
 
+const UtilityCard = ({ card }) => {
+  const [src, { blur }] = useProgressiveImg(
+    "images/utility/tiny.jpg",
+    card.imgSrc
+  );
+
+  return (
+    <div className="relative flex-col flex w-full">
+      <div className="border-stone-30 border-2 relative">
+        <div style={{ minHeight: "500px" }}>
+          <img
+            src={src}
+            className="w-full"
+            style={{
+              filter: blur ? "blur(20px)" : "none",
+              transition: blur ? "none" : "filter 0.3s ease-out",
+            }}
+            alt="Utility"
+          />
+        </div>
+        <div className="flex pb-4 absolute bottom-0 pl-2 backdrop-blur-xl w-full">
+          <div className="flex flex-col">
+            <h1 className="w-5/6 py-2 font-semibold">{card.title}</h1>
+            <table className="text-xs">
+              {card.detailTable.map((row) => (
+                <tr>
+                  <td className="pr-3 font-extralight opacity-100">
+                    {row.key}
+                  </td>
+                  <td>{row.value}</td>
+                </tr>
+              ))}
+            </table>
+          </div>
+          <h1 className="text-3xl md:text-7xl font-bold ml-auto mr-1">
+            {card.number}
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Utility = () => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -83,36 +128,12 @@ function Utility() {
         </div>
         <div class="grid xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 xl:gap-24 md:gap-20 mt-8">
           {utilityCards.map((card) => (
-            <div className="relative flex-col flex w-full">
-              <div className="border-stone-30 border-2 relative">
-                <div style={{ minHeight: "500px" }}>
-                  <img src={card.imgSrc} alt="NFT" className="w-full" />
-                </div>
-                <div className="flex pb-4 absolute bottom-0 pl-2 backdrop-blur-xl w-full">
-                  <div className="flex flex-col">
-                    <h1 className="w-5/6 py-2 font-semibold">{card.title}</h1>
-                    <table className="text-xs">
-                      {card.detailTable.map((row) => (
-                        <tr>
-                          <td className="pr-3 font-extralight opacity-100">
-                            {row.key}
-                          </td>
-                          <td>{row.value}</td>
-                        </tr>
-                      ))}
-                    </table>
-                  </div>
-                  <h1 className="text-3xl md:text-7xl font-bold ml-auto mr-1">
-                    {card.number}
-                  </h1>
-                </div>
-              </div>
-            </div>
+            <UtilityCard card={card}></UtilityCard>
           ))}
         </div>
       </div>
     </motion.div>
   );
-}
+};
 
 export default Utility;
