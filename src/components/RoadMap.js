39434c1090f15ea40a40f2414717b3c7010625ff
Eric Thomas <bluebird6922@gmail.com>
2022-09-09 11:36:08 +0300

    update roadmap

--- src/components/RoadMap.js
+++ src/components/RoadMap.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./RoadMap.css";
 import { motion } from "framer-motion";
 
@@ -64,8 +65,6 @@ const roadMapCards1 = [
     paras: [
       `This is where the true value of The Chosen Ones lays. Ever since a young age Michael Pieri (Founder & CEO) has always been about empowering others, supporting those in need, and building others up in the areas he had experience in to provide more confidence to the individual. The Chosen Ones is a way Michael can continue to do this on a much larger scale, in others words globally.`,
       `To kick of stage 4 of our roadmap we will be hosting a 2 day event with our partners over at Club 14. The itinerary is yet to be released but expect world class speakers, entreprneurs, real estate gurus, investment and crypto experts, musicians, food, drinks, and much more. Everything will be provided by Nine Lives and Club 14 and will be the biggest event of the year, so don’t miss out.`,
-      `Following this event will be the release of our Accelerator Program - Create to Earn. We will have an application process for anyone to apply where we will carefully select the top few businesses and/or individuals based on very specific criteria only known to us. We want you to apply for this application with everything you’ve got, we want no stone left unturned. After we have selected the winning applicants, we will then begin the meetings and planning process to see how we can further assist your ideas from funding, training, advising, and more.`,
-      `“Give a Man a Fish, and You Feed Him for a Day. Teach a Man To Fish, and You Feed Him for a Lifetime”. This rings true to everything Michael and his team are about and with this program not only will we help fund or develop your ideas, we will ensure that educating you across all facets of business ownership, leadership, taxes and more to ensure your future success once you leave our program.`,
     ],
     imgsrc: "images/roadmap/roamap-1.png",
   },
@@ -98,8 +97,6 @@ const roadMapCards2 = [
     paras: [
       `This is where the true value of The Chosen Ones lays. Ever since a young age Michael Pieri (Founder & CEO) has always been about empowering others, supporting those in need, and building others up in the areas he had experience in to provide more confidence to the individual. The Chosen Ones is a way Michael can continue to do this on a much larger scale, in others words globally.`,
       `To kick of stage 4 of our roadmap we will be hosting a 2 day event with our partners over at Club 14. The itinerary is yet to be released but expect world class speakers, entreprneurs, real estate gurus, investment and crypto experts, musicians, food, drinks, and much more. Everything will be provided by Nine Lives and Club 14 and will be the biggest event of the year, so don’t miss out.`,
-      `Following this event will be the release of our Accelerator Program - Create to Earn. We will have an application process for anyone to apply where we will carefully select the top few businesses and/or individuals based on very specific criteria only known to us. We want you to apply for this application with everything you’ve got, we want no stone left unturned. After we have selected the winning applicants, we will then begin the meetings and planning process to see how we can further assist your ideas from funding, training, advising, and more.`,
-      `“Give a Man a Fish, and You Feed Him for a Day. Teach a Man To Fish, and You Feed Him for a Lifetime”. This rings true to everything Michael and his team are about and with this program not only will we help fund or develop your ideas, we will ensure that educating you across all facets of business ownership, leadership, taxes and more to ensure your future success once you leave our program.`,
     ],
     imgsrc: "images/roadmap/roamap-1.png",
   },
@@ -135,6 +132,96 @@ const roadMapCards2 = [
   },
 ];
 
+const RoadMapItem = ({ card, onClose }) => {
+  return (
+    <div className="flex justify-center items-center fixed inset-0 z-50 outline-none focus:outline-none bg-black">
+      <div className="relative">
+        <div className="absolute w-full roadmap-card-bg"></div>
+        <div className="relative roadmap-item-container">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none dark:bg-gray-700">
+            <div className="relative flex-auto">
+              <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4">
+                <div className="color-white p-12">
+                  <h1 className="roadmap-card-number">{card.cardNumber}</h1>
+                  <article className="mt-8">
+                    <h2 className="roadmap-item-title mb-4">
+                      {card.phaseString}
+                    </h2>
+                    <h2 className="roadmap-item-title">{card.title}</h2>
+                    <h3 className="roadmap-card-subtitle">{card.subtitle}</h3>
+                    {card.paras.map((para) => (
+                      <p className="pt-4">{para}</p>
+                    ))}
+                  </article>
+                </div>
+                <div className="relative">
+                  <div className="absolute top-1 right-1">
+                    <button
+                      type="button" 
+                      className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                      onClick={() => onClose()}
+                    >
+                      <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                      <span class="sr-only">Close modal</span>
+                    </button>
+                  </div>
+                  <img
+                    src={card.cardImage}
+                    alt=""
+                    className="roadmap-item-image"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const RoadMapScene = ({ cards }) => {
+  const [card, setCard] = useState(null);
+  const openCardDetail = (card) => {
+    console.log("openCardDetail", card);
+    setCard(card);
+  };
+
+  return (
+    <div className="relative">
+      <div className="absolute w-full roadmap-card-bg"></div>
+      <div className="sticky">
+        <div className="grid grid-flow-col grid-cols-1 md:grid-cols-2 md:grid-rows-2 md:gap-4 py-12 px-72">
+          {cards.map((card, index) => (
+            <div
+              className="roadmap-card border border-black grid grid-cols-2"
+              onClick={() => openCardDetail(card)}
+            >
+              <div className="p-4">
+                <h1 className="roadmap-card-number">{card.cardNumber}</h1>
+                <article className="mt-8">
+                  <h2 className="roadmap-card-title">{card.cardTitle}</h2>
+                  <h2 className="roadmap-card-subtitle">{card.cardSubtitle}</h2>
+                </article>
+              </div>
+              <div className="flex justify-end">
+                <img
+                  src={card.cardImage}
+                  alt="Roadmap"
+                  className="min-h-full"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {!!card && (
+        <RoadMapItem card={card} onClose={() => setCard(null)}></RoadMapItem>
+      )}
+    </div>
+  );
+};
+
 function RoadMap() {
   return (
     <motion.div
@@ -146,8 +233,9 @@ function RoadMap() {
         <div>
           <h1 className="title">ROADMAP</h1>
         </div>
-        <div className="sub_container">
-          {/* {roadMapCards1.map((card, index) => (
+      </div>
+      <div className="">
+        {/* {roadMapCards1.map((card, index) => (
             <div className={`sub_items`}>
               <div className={`sub_item `}>
                 <div className="sub_half">
@@ -167,25 +255,8 @@ function RoadMap() {
               </div>
             </div>
           ))} */}
-          <div class="grid md:grid-cols-2 sm:grid-cols-1 md:gap-4 py-12 px-48">
-            {roadMapCards1.map((card, index) => (
-              <div className="roadmap-card grid grid-cols-2">
-                <div className="p-4">
-                  <h1 className="roadmap-card-number">{card.cardNumber}</h1>
-                  <article className="mt-8">
-                    <h2 className="roadmap-card-title">{card.cardTitle}</h2>
-                    <h2 className="roadmap-card-subtitle">
-                      {card.cardSubtitle}
-                    </h2>
-                  </article>
-                </div>
-                <div className="sub_half">
-                  <img src={card.cardImage} alt="Roadmap" className="w-full" />
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <RoadMapScene cards={roadMapCards1}></RoadMapScene>
+        <RoadMapScene cards={roadMapCards2}></RoadMapScene>
       </div>
     </motion.div>
   );
